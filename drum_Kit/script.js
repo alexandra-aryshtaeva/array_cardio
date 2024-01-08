bbec2d88b5999afa5302884b5c3f89f66339060d
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2024-01-08 12:32:17 +0000

    finisehd

--- drum_Kit/script.js
+++ drum_Kit/script.js
@@ -3,9 +3,23 @@
 //   // document.querySelector("p").innerHTML += "you clicked " + ev.key + "<br>";
 // });
 
-window.addEventListener("keydown", (e) => {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  const key = document.querySelector(`.key[data-key="${e.code}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-});
+  key.currentTime = 0;
+  key.classList.add("playing");
+}
+
+function removeTRansition(e) {
+  console.log(e);
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTRansition));
+
+window.addEventListener("keydown", playSound);
