edc827f8c5d4ca37bd1e29b75f14fd56faa8e16a
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2024-01-11 10:47:41 +0000

    after adding animations to the top and bottom words with css, I connected the trigger with java script

---
+++ 5_gallery/script.js
@@ -0,0 +1,18 @@
+let panels = document.querySelectorAll(".panel");
+
+function toggleOPen() {
+  this.classList.toggle("open");
+}
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", toggleOPen);
+});
+
+function toggleActive(e) {
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+panels.forEach((panel) => {
+  panel.addEventListener("transitionend", toggleActive);
+});
