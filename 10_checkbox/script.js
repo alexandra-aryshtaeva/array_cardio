0cabd11b5839e938e3a5ddf07a871032d74adc21
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2024-01-12 19:26:22 +0000

    checkbox hard

--- 10_checkbox/script.js
+++ 10_checkbox/script.js
@@ -1,10 +1,4 @@
 let checkboxes = document.querySelectorAll(`input[type="checkbox"]`);
 let item = document.querySelectorAll(".item");
-
-checkboxes.forEach((e) => {
-  e.addEventListener("click", () => {
-    item.classList.add.("checked");
-    // item.classList.toggle("checked");
-  });
-});
-console.log(checkboxes);
+let shiftPressed = false;
+let lastActiveCheckbox;
